6ad346f131552ae9eedcbbd426777ac204023b6b
Teatoller <steven.otieno1@gmail.com>
2019-12-13 13:50:59 +0300

    (update): Implement student update method
    - add method to update student record
    
    [Starts #008]

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -84,7 +84,21 @@ class ApiController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->name = is_null($request->name) ? $student->name : $request->name;
+            $student->course = is_null($request->course) ? $student->course : $request->course;
+            $student->save();
+
+            return response()->json([
+                "message" => "records updated successfully",
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "Student not found",
+            ], 404);
+
+        }
     }
 
     /**
